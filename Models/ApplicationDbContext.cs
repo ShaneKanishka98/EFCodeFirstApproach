8569b424870ed8014a4b66c048828ae27a309d48
ShaneKanishka98 <shanekanishka1998@gmail.com>
2024-04-24 16:15:52 +0530

    Code First Approach

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace coreEFCodeFirstApproach.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer("Server=LAPTOP-75O0631G;Database=CodeFirstDB;Integrated Security=true;TrustServerCertificate=true;");
+        }
+    }
+}
